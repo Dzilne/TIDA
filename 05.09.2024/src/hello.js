6afeb4e2b8c49689d9c87f77dfd1afdc8e049532
Dzilne <90028788+Dzilne@users.noreply.github.com>
2024-09-05 13:11:01 +0200

    Delete 05.09.2024 directory

--- 05.09.2024/src/hello.js
+++
@@ -1,8 +0,0 @@
-/**
- * @description Funkcja przyjmuje parametr string o nazwie somabody i zwraca alert Hello {somebody}.
- * @param somebody
- * @author Krzysztof Dzilne 4D
- */
-const sayHello = (somebody) => {
-    alert(`Hello ${somebody}`);
-};
